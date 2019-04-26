5a851bb726281bca32ed3e2392d7c68beec52207
ldblovepeace <853126656@qq.com>
2019-04-26 15:58:50 +0800

    C # learning for new class

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "      Hello World!       ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+        }
+    }
+}
